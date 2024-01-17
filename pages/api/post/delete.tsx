8188c8fc5d94a84015d4a8292d88d9ed6ad0ca04
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2024-01-18 00:52:40 +0900

    [feat] delete 로직 변경

--- pages/api/post/delete.tsx
+++ pages/api/post/delete.tsx
@@ -3,22 +3,29 @@ import { ObjectId } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const Delete = async (request: NextApiRequest, response: NextApiResponse) => {
-  if (request.method == "DELETE"){
-  const db = (await connectDB).db("board");
-
-  let result = await db.collection("post").deleteOne(
-    {
-      _id: new ObjectId(request.body.id),
-    },
-    { writeConcern: { w: "majority" } }
-  );
-  console.log(result);
-
-  response.status(302).redirect("/list");
+  if (request.method == "DELETE") {
+    try {
+      const db = (await connectDB).db("board");
+      // parse를 통해 json 형태로 변환
+      let id = JSON.parse(request.body).id;
+      let result = await db.collection("post").deleteOne(
+        {
+          _id: new ObjectId(id),
+        },
+        { writeConcern: { w: "majority" } }
+      );
+      console.log("result는 ", result);
+      if (result.deletedCount === 1) {
+        response.status(302).redirect("/list");
+      } else {
+        response.status(400).json({ message: "fail" });
+      }
+    } catch (e) {
+      response.status(500).json({ message: "error" });
+    }
   } else {
     response.status(400).json("잘못된 접근입니다.");
   }
-
 };
 
 export default Delete;
